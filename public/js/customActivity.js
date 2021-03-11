1d91c9ff2f4f24fb71570dfe2276c4148df73d13
360DegreeCloudPVTLTD <64471988+360DegreeCloudPVTLTD@users.noreply.github.com>
2021-03-11 13:46:06 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -100,7 +100,7 @@ define([
             "authToken": authToken,
             "messagingService": messagingService,
             "body": body,
-            "to": "{{Contact.Attribute.Lead.Phone}}" //<----This should map to your data extension name and phone number column {{Contact.Attribute.TwilioV1.TwilioNumber}}
+            "to": "{{Contact.Attribute.testingmanisha.Phone}}" //<----This should map to your data extension name and phone number column {{Contact.Attribute.TwilioV1.TwilioNumber}}
         }];
 
         payload['metaData'].isConfigured = true;
